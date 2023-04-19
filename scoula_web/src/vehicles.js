013b282bd3821368a8f589b44177beaa1b5d32f8
ashenanu <ashenanu09@gmail.com>
2023-04-19 13:52:14 +0530

    importing style

--- scoula_web/src/vehicles.js
+++ scoula_web/src/vehicles.js
@@ -1,7 +1,6 @@
 import React,{useEffect,useState} from "react";
 import axios from 'axios';
 axios.defaults.baseURL= "http://localhost:8080";
-
 const ViewVehicles= () =>{
     const [vehicles, setVehicles]= useState([]);
     useEffect(()=>{
